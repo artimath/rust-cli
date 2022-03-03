360679df631386698a62df9013e545517851bed8
artimath <ryanjhunter@gmail.com>
2022-03-02 18:32:15 -0600

    my args versions

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 
+use clap::{App, Arg};
+
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
     Ok(())
 }
 
@@ -14,6 +16,43 @@ pub struct Config {
     number_nonblank_lines: bool,
 }
 
-pub fn get_args() -> MyResult<Config> {}
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Ryan Hunter")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("file")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .default_value("-")
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("number")
+                .conflicts_with("number_nonblank")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number nonblank lines"),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: if matches.is_present("file") {
+            matches.values_of_lossy("file").unwrap()
+        } else {
+            vec!["-".to_string()]
+        },
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number-nonblank"),
+    })
+}
 
 // pub fn get_args() -> My
